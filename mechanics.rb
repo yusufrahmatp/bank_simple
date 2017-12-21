4d4e7d3872e9869836631b4a53e52d56972d84b0
Nicholas R.P <nicholasmagbanua@gmail.com>
2017-12-21 17:30:04 +0700

    login add withdraw

--- mechanics.rb
+++ mechanics.rb
@@ -1,20 +1,24 @@
 Account = Struct.new(:name, :pin, :address, :balance, :active)
 @acc_data = Hash.new(nil)
+@cur_acc = nil
 
 def create_account
   loop do
     @acc = Random.rand(1000..9999)
     break if @acc_data[@acc.to_s.to_sym].nil?
   end
+  @cur_acc=@acc.to_s.to_sym
   print "Name: "
   name = gets.chomp
+  require 'io/console'
   print "PIN: "
-  pin = gets.chomp
+  pin = STDIN.noecho(&:gets).chomp
+  puts
   print "Address: "
   address = gets.chomp
   print "Initial Balance: "
   balance = gets.chomp
-  @acc_data[@acc.to_s.to_sym] = Account.new(name,pin.to_i,address,balance,true)
+  @acc_data[@acc.to_s.to_sym] = Account.new(name,pin.to_i,address,balance.to_i,true)
 end
 
 def admin_print_data
@@ -29,7 +33,58 @@ def admin_print_data
 end
 
 
-  puts "Bikin acc"
-  create_account
-  create_account
-  admin_print_data
+def add_balance(noacc)
+  print "Balance : "
+  loop do
+    addbalance = Integer(gets.chomp) rescue false
+    break if addbalance
+    puts "Invalid input"
+  end
+  @acc_data[noacc][:balance]+=addbalance.to_i
+  puts "Success"
+end
+
+
+def withdraw_balance(noacc)
+  loop do
+    print "Balance : "
+    loop do
+      withdraw = Integer(gets.chomp) rescue false
+      break if withdraw
+      puts "Invalid input"
+    end
+    break if withdraw <= @acc_data[noacc][:balance]
+    puts "Balance isn't enough"
+  end
+  @acc_data[noacc][:balance]-=withdraw
+  puts "Withdraw success"
+end
+
+def login
+  chance =Hash.new(3)
+  loop do
+    loop do
+      print "No. Acc : "
+      noacc = Integer(gets.chomp) rescue false
+      break if noacc && !@acc_data[noacc.to_i.to_sym].nil?
+      puts "Invalid account number"
+    end
+    chance[noacc.to_i.to_sym]=3
+    print "PIN : "
+    pin = Integer(gets.chomp) rescue false
+    break if pin==@acc_data[noacc.to_i.to_sym][:pin]
+    chance[noacc.to_i.to_sym] -= 1
+    if chance[noacc.to_i.to_sym]==0
+      puts "Account is blocked"
+    else
+      puts "Invalid pin, Chance remaining : #{chance[noacc.to_i.to_sym]}"
+    end
+  end
+  if chance[noacc.to_i.to_sym]>0
+    @cur_acc=noacc.to_i.to_sym
+    puts "Login success"
+  else
+    puts "Login failed"
+  end
+end
+
