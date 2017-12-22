e66b2822f7e08615e36eb4d95dd40db55fde57e1
yusufrahmatp <32531272+SoarTheSky@users.noreply.github.com>
2017-12-22 11:14:59 +0700

    Add class

--- mechanics.rb
+++ mechanics.rb
@@ -1,7 +1,61 @@
 require 'io/console'
 
+class Account
+  def initialize(name,pin,address,balance,active)
+    @name = name
+    @pin = pin.to_i
+    @address = address
+    @balance = balance.to_i
+    @active = active
+    @log = []
+  end
 
-Account = Struct.new(:name, :pin, :address, :balance, :active)
+  def name
+    @name
+  end
+
+  def pin
+    @pin
+  end
+
+  def address
+    @address
+  end
+
+  def balance
+    @balance
+  end
+
+  def active
+    @active
+  end
+
+  def mutation
+    @log
+  end
+
+  def insertmutation(type, amount)
+    @log.unshift("#{type.upcase}  #{amount}")
+    @log.pop if @log.length > 5
+  end
+
+  def edit(x, data)
+    case x
+    when "name"
+      @name = data
+    when "pin"
+      @pin = data
+    when "address"
+      @address = data
+    when "balance"
+      @balance = data
+    when "active"
+      @active = data
+    else
+    end
+  end
+end
+
 @acc_data = Hash.new(nil)
 @cur_acc = nil
 
@@ -21,7 +75,7 @@ def create_account
   address = gets.chomp
   print "Initial Balance: "
   balance = gets.chomp
-  @acc_data[@acc.to_s.to_sym] = Account.new(name,pin.to_i,address,balance.to_i,true)
+  @acc_data[@acc.to_s.to_sym] = Account.new(name,pin,address,balance,true)
   puts "Your account has been created with account number #{@acc}"
 end
 
@@ -30,25 +84,39 @@ def admin_print_data
     puts "----------------------------------------------------"
     puts "------------------Account no. #{x}------------------"
     puts "----------------------------------------------------"
-    puts "Name      :  #{@acc_data[x][:name]}"
-    puts "PIN Number:  #{@acc_data[x][:pin]}"
-    puts "Address   :  #{@acc_data[x][:address]}"
-    puts "Balance   :  Rp #{@acc_data[x][:balance]}"
-    puts "Active    :  #{@acc_data[x][:active]}"
+    puts "Name      :  #{@acc_data[x].name}"
+    puts "PIN Number:  #{@acc_data[x].pin}"
+    puts "Address   :  #{@acc_data[x].address}"
+    puts "Balance   :  Rp #{@acc_data[x].balance}"
+    puts "Active    :  #{@acc_data[x].active}"
     puts "----------------------------------------------------"
   end
 end
 
-def print_data (acc)
+def print_mutation (acc)
+    mutation = @acc_data[acc].mutation
     puts "----------------------------------------------------"
     puts "------------------Account no. #{acc.to_s}------------------"
     puts "----------------------------------------------------"
-    puts "Name      :  #{@acc_data[acc][:name]}"
-    puts "PIN Number:  #{@acc_data[acc][:pin]}"
-    puts "Address   :  #{@acc_data[acc][:address]}"
-    puts "Balance   :  Rp #{@acc_data[acc][:balance]}"
-    puts "Active    :  #{@acc_data[acc][:active]}"
+    if mutation.nil?
+      puts "Account mutation empty!"
+    else
+      puts "From most recent: "
+      mutation.each {|x| puts x}
+    end
     puts "----------------------------------------------------"
+end
+
+def print_data (acc)
+  puts "----------------------------------------------------"
+  puts "------------------Account no. #{acc.to_s}------------------"
+  puts "----------------------------------------------------"
+  puts "Name      :  #{@acc_data[acc].name}"
+  puts "PIN Number:  #{@acc_data[acc].pin}"
+  puts "Address   :  #{@acc_data[acc].address}"
+  puts "Balance   :  Rp #{@acc_data[acc].balance}"
+  puts "Active    :  #{@acc_data[acc].active}"
+  puts "----------------------------------------------------"
 end
 
 def edit_data (acc)
@@ -65,7 +133,7 @@ def edit_data (acc)
         puts "Insert your new name"
         print ">> "
         input1 = gets.chomp.capitalize
-        @acc_data[acc][:name] = input1
+        @acc_data[acc].edit(input, input1)
         puts "Name successfuly changed to #{input1}!"
 
       when "pin"
@@ -76,14 +144,14 @@ def edit_data (acc)
           break if input1
           puts "Invalid input!"
         end
-        @acc_data[acc][:pin] = input1
+        @acc_data[acc].edit(input, input1)
         puts "PIN successfuly changed to #{input1}!"
 
       when "address"
         puts "Insert your new address "
         print ">> "
         input1 = gets.chomp.capitalize
-        @acc_data[acc][:address] = input1
+        @acc_data[acc].edit(input, input1)
         puts "Address successfuly changed to #{input1}"
 
       else
@@ -95,7 +163,7 @@ end
 
 
 def block_account (acc)
-  @acc_data[acc][:active] = false
+  @acc_data[acc].edit("active", false)
 end
 
 
@@ -106,8 +174,9 @@ def add_balance(noacc)
     break if @addbalance
     puts "Invalid input"
   end
-  @acc_data[noacc][:balance]+=@addbalance
-  puts "Success, Balance : #{@acc_data[noacc][:balance]}"
+  @acc_data[noacc].edit("balance",(@acc_data[noacc].balance + @addbalance))
+  @acc_data[noacc].insertmutation("K",@addbalance)
+  puts "Success, Balance : #{@acc_data[noacc].balance}"
 end
 
 
@@ -119,10 +188,11 @@ def withdraw_balance(noacc)
       break if @withdraw
       puts "Invalid input"
     end
-    break if @withdraw <= @acc_data[noacc][:balance]
+    break if @withdraw <= @acc_data[noacc].balance
     puts "Balance isn't enough"
   end
-  @acc_data[noacc][:balance]-=@withdraw
+  @acc_data[noacc].edit("balance",(@acc_data[noacc].balance - @withdraw))
+  @acc_data[noacc].insertmutation("D",@withdraw)
   puts "Withdraw success"
 end
 
@@ -136,22 +206,22 @@ def login
       break if @noacc && !@acc_data[@noacc.to_s.to_sym].nil?
       puts "Invalid account number"
     end
-    if @acc_data[@noacc.to_s.to_sym][:active]
+    if @acc_data[@noacc.to_s.to_sym].active
       print "PIN : "
       pin = Integer(gets.chomp) rescue false
-      break if pin==@acc_data[@noacc.to_s.to_sym][:pin]
+      break if pin==@acc_data[@noacc.to_s.to_sym].pin
       chance[@noacc.to_s.to_sym] -= 1
       if chance[@noacc.to_s.to_sym]==0
         puts "Account is blocked"
-        @acc_data[@noacc.to_s.to_sym][:active]=false
+        @acc_data[@noacc.to_s.to_sym].active=false
       else
         puts "Invalid pin, Chance remaining : #{chance[@noacc.to_s.to_sym]}"
       end
     end
-    break if !@acc_data[@noacc.to_s.to_sym][:active]
+    break if !@acc_data[@noacc.to_s.to_sym].active
   end
   if chance[@noacc.to_s.to_sym]>0
-    if @acc_data[@noacc.to_s.to_sym][:active]
+    if @acc_data[@noacc.to_s.to_sym].active
       @cur_acc=@noacc.to_s.to_sym
       puts "Login success"
     else
@@ -184,25 +254,28 @@ loop do
   if (@cur_acc.nil?)
     "Exit..."
   else
-    puts "Welcome #{@acc_data[@cur_acc][:name]}"
+    puts "Welcome #{@acc_data[@cur_acc].name}"
     loop do
       puts "Menu"
       puts "1. check"
-      puts "2. add"
-      puts "3. withdraw"
-      puts "4. edit"
-      puts "5. freeze"
-      puts "6. logout"
-      puts "7. exit"
+      puts "2. mutation"
+      puts "3. add"
+      puts "4. withdraw"
+      puts "5. edit"
+      puts "6. freeze"
+      puts "7. logout"
+      puts "8. exit"
       loop do
         print ">> "
         @input2 = gets.chomp.downcase
-        break if @input2=="freeze"||@input2=="check"||@input2=="add"||@input2=="withdraw"||@input2=="edit"||@input2=="logout"||@input2=="exit"
+        break if @input2=="freeze"||@input2=="check"||@input2=="mutation"||@input2=="add"||@input2=="withdraw"||@input2=="edit"||@input2=="logout"||@input2=="exit"
         puts "Invalid input"
       end
       case @input2
       when "check"
         print_data(@cur_acc)
+      when "mutation"
+        print_mutation(@cur_acc)
       when "add"
         add_balance(@cur_acc)
       when "edit"
